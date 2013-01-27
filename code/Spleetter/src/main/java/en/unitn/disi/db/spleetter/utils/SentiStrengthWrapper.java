9bda3bbec51ad99bf7347f8b289fa31da313e123
Kuzeko <web@kuzeko.com>
2013-01-27 17:42:19 +0100

    refactored utils package

---
+++ code/Spleetter/src/main/java/en/unitn/disi/db/spleetter/utils/SentiStrengthWrapper.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package en.unitn.disi.db.spleetter.utils;
+
+import uk.ac.wlv.sentistrength.SentiStrength;
+
+/**
+ * Wrapper for the SentiStrength library
+ *
+ */
+public class SentiStrengthWrapper {
+
+    private SentiStrength classifier;
+
+    /** The unique instance **/
+    private volatile static SentiStrengthWrapper instance;
+
+    /** The private constructor **/
+    private SentiStrengthWrapper() {
+        String[] args = {"sentidata", "/tmp/SentiStrength_Data/", "text", "i don't hate you. I really hate you"};
+        classifier = new SentiStrength(args);
+    }
+
+    public static SentiStrengthWrapper getInstance() {
+      if (instance == null) {
+          synchronized(SentiStrengthWrapper.class) {
+             if (instance == null) {
+                instance = new SentiStrengthWrapper();
+             }
+          }
+       }
+       return instance;
+    }
+
+
+    public double[] analyze(String text){
+        double[] polarities = new double[2];
+
+        String result = this.classifier.computeSentimentScores(text);
+        String [] tokens = result.split(" ");
+        polarities[0] = Double.parseDouble(tokens[1]);
+        polarities[1] = Double.parseDouble(tokens[0]);
+        return polarities;
+    }
+}
---
+++ code/Spleetter/src/main/java/en/unitn/disi/db/spleetter/utils/TweetCleanseMain.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package en.unitn.disi.db.spleetter.utils;
+
+/**
+ *
+ * NO-OP        
+ */
+public class TweetCleanseMain {
+    
+    public static void main(String[] args){
+        //no op        
+    }
+}
