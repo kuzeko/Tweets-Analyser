ad0cc2fe947729de5d4445ee04158a77e4973b2a
Kuzeko <web@kuzeko.com>
2013-02-07 13:48:40 +0100

    changed generation of PactRecord

--- code/Spleetter/src/main/java/eu/unitn/disi/db/spleetter/match/TweetPolarityMatch.java
+++ code/Spleetter/src/main/java/eu/unitn/disi/db/spleetter/match/TweetPolarityMatch.java
@@ -5,7 +5,9 @@ import eu.stratosphere.pact.common.stubs.MatchStub;
 import eu.stratosphere.pact.common.stubs.StubAnnotation;
 import eu.stratosphere.pact.common.type.PactRecord;
 import eu.stratosphere.pact.common.type.base.PactDouble;
+import eu.stratosphere.pact.common.type.base.PactInteger;
 import eu.stratosphere.pact.common.type.base.PactLong;
+import eu.stratosphere.pact.common.type.base.PactString;
 
 /**
  * Joins the cleaned english tweets with their correspondent polarities
@@ -22,17 +24,18 @@ import eu.stratosphere.pact.common.type.base.PactLong;
 @StubAnnotation.ConstantFieldsFirst(fields = {0,1,2,3,4})
 @StubAnnotation.OutCardBounds(lowerBound = 1, upperBound = 1)
 public class TweetPolarityMatch extends MatchStub {
-    private PactDouble negPolarity = new PactDouble();
-    private PactDouble posPolarity = new PactDouble();
-
+    private PactRecord output = new PactRecord(7);
     @Override
     public void match(PactRecord englishTweet, PactRecord polarityValue, Collector<PactRecord> records) throws Exception {
 
-        negPolarity = polarityValue.getField(2, PactDouble.class);
-        posPolarity = polarityValue.getField(3, PactDouble.class);
-        englishTweet.setField(5, negPolarity);
-        englishTweet.setField(6, posPolarity);
-        records.collect(englishTweet);
+        output.setField(0, englishTweet.getField(0, PactLong.class));
+        output.setField(1, englishTweet.getField(1, PactInteger.class));
+        output.setField(2, englishTweet.getField(2, PactString.class));
+        output.setField(3, englishTweet.getField(3, PactInteger.class));
+        output.setField(4, englishTweet.getField(4, PactString.class));
+        output.setField(5, polarityValue.getField(2, PactDouble.class));
+        output.setField(6, polarityValue.getField(3, PactDouble.class));
+        records.collect(output);
     }
 
 }
