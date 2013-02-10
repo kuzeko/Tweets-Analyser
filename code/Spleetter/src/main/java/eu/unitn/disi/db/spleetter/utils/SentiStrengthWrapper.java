c0fdd6fcd49e0e3a35286e8911af7bb493f9a326
Kuzeko <web@kuzeko.com>
2013-02-10 11:57:15 +0100

    moved classes in the right package
    is EU not EN

--- code/Spleetter/src/main/java/eu/unitn/disi/db/spleetter/utils/SentiStrengthWrapper.java
+++ code/Spleetter/src/main/java/eu/unitn/disi/db/spleetter/utils/SentiStrengthWrapper.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package en.unitn.disi.db.spleetter.utils;
+package eu.unitn.disi.db.spleetter.utils;
 
 import uk.ac.wlv.sentistrength.SentiStrength;
 
--- code/Spleetter/src/main/java/eu/unitn/disi/db/spleetter/utils/TweetCleanseMain.java
+++ code/Spleetter/src/main/java/eu/unitn/disi/db/spleetter/utils/TweetCleanseMain.java
@@ -2,15 +2,15 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package en.unitn.disi.db.spleetter.utils;
+package eu.unitn.disi.db.spleetter.utils;
 
 /**
  *
- * NO-OP        
+ * NO-OP
  */
 public class TweetCleanseMain {
-    
+
     public static void main(String[] args){
-        //no op        
+        //no op
     }
 }
